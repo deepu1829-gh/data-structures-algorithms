99284293f995cfe7fda36349626c445e2e996860
Anudeep Ch <anu.deepu08@gmail.com>
2025-11-09 12:40:31 +0530

    height of tree

---
+++ trees/height/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+// Node structure
+type Node struct {
+	value int
+	left  *Node
+	right *Node
+}
+
+// Helper recursive function
+func preorder(root *Node) int {
+	if root == nil {
+		return 0
+	}
+
+	return 1 + preorder(root.left) + preorder(root.right)
+}
+
+func newNode(value int) *Node {
+	return &Node{value: value}
+}
+
+func main() {
+	/*
+	        4
+	      /   \
+	     2     6
+	    / \   / \
+	   1  3  5  7
+	*/
+
+	root := newNode(4)
+	root.left = newNode(2)
+	root.right = newNode(6)
+	root.left.left = newNode(1)
+	root.left.right = newNode(3)
+	root.right.left = newNode(5)
+	root.right.right = newNode(7)
+
+	result := preorder(root)
+	fmt.Println("In-order result:", result)
+}
